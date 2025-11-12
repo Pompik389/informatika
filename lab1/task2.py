1a665f9d1fb61d863e1eeb1734caa6f17145e2f4
Pompik389 <shubin25rab@gmail.com>
2025-11-12 11:48:12 +0500

    Update task2.py
    Obnovil task2

--- lab1/task2.py
+++ lab1/task2.py
@@ -1,1 +1,8 @@
-
+from math import sin, cos, log, exp
+x = float(input("введите значение переменной x: "))
+y = float(input("введите значение переменной y: "))
+z = float(input("введите значение переменной z: "))
+a=(x**3/2)**0.5-sin(y)
+b=exp(2)/3-cos(y)+z+log(y)
+print(f"Получено значение функции a={a}")
+print(f"Получено значение функции b={b}")
